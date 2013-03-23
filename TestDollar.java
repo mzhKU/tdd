16d597d7dba8e1086107deb86dfd5b1201ae263f
mzh <ma.hed@bluewin.ch>
2013-03-23 11:28:59 +0100

    Added main().

--- TestDollar.java
+++ TestDollar.java
@@ -3,6 +3,11 @@ import static org.junit.Assert.*;
 
 public class TestDollar
 {
+    public static void main(String[] args)
+    {
+    System.out.println("Main.");
+    }
+
     public void TestMultiplication()
     {
         Dollar five = new Dollar(5);
