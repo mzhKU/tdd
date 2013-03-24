9695230001d4d0ee6f8b60949ecf274e872677f2
mzh <ma.hed@bluewin.ch>
2013-03-24 14:13:01 +0100

    Renamed Dollar.java to Money.java

--- TestDollar.java
+++ TestDollar.java
@@ -13,17 +13,17 @@ public class TestDollar
     public void testFrancMultiplication()
     {
         Money franc = Money.franc(5);
-    //    assertEquals(Money.franc(10), five.times(2));
-    //    assertEquals(Money.franc(15), five.times(3));
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 
     public void testEquality()
     {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        //assertTrue(Money.franc(5).equals(Money.franc(5)));
-        //assertFalse(Money.franc(5).equals(Money.franc(6)));
-        //assertFalse(Money.franc(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     public static void main(String[] args)
